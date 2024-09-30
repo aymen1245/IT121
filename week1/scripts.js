57ae282fce785133e2e1b554af12eb7bb5aee1cd
aymen1245 <Aymen.Khaleefa@seattlecolleges.edu>
2024-09-29 22:37:54 -0700

    Create scripts.js

---
+++ week1/scripts.js
@@ -0,0 +1,4 @@
+const firstName = "YourFirstName"; // Replace with your first name
+const lastName = "YourLastName"; // Replace with your last name
+const yearsOfStudy = 4; // Replace with your years of study
+const goal = "become a web developer"; // Replace with your goal
