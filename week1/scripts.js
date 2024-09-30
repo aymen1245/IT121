96b4e28573bac71846e9acf29046eccc1d840893
aymen1245 <Aymen.Khaleefa@seattlecolleges.edu>
2024-09-29 22:40:09 -0700

    Update scripts.js

--- week1/scripts.js
+++ week1/scripts.js
@@ -1,4 +1,4 @@
-const firstName = "YourFirstName"; // Replace with your first name
-const lastName = "YourLastName"; // Replace with your last name
-const yearsOfStudy = 4; // Replace with your years of study
-const goal = "become a web developer"; // Replace with your goal
+const firstName = "Aymen";   
+const lastName = "Khaleefa"; 
+const yearsOfStudy = 2;  
+const goal = "become a web designerr";  
